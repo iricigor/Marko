fb5dd919ef458386896fa9b1d4c0efc60ae3896b
Igor Iric <iricigor@outlook.com>
2019-04-02 20:30:32 +0200

    day 1

---
+++ ahoj.py
@@ -0,0 +1,30 @@
+# ovo je napisao Igor
+print("Ahoj Petre")
+# ovo je napisao Petar
+print("Ahoj Marko")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#ovo je napisao Dusan 
+a = 10
+print(a)
+a = 12
+print(a)
+b = a + x
+print (b)
+x = 9
+x = "Jack"
+print(x)
+
+print(a+5)
+# print ("x+5=?")
+# print(x+5)
+
+print("7*12")
+print(7*12)
+
+
+
