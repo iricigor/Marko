77e627a7a9a882f371bdf01d06bb120e706fbf4c
Igor Iric <iricigor@outlook.com>
2019-04-07 09:28:21 +0200

    20190407

---
+++ M-ukol1.py
@@ -0,0 +1,9 @@
+# ctvercova cisla do 100
+
+print ("Ctvercova cisla do 100")
+#1x1=1
+#2x2=4
+#3x3=9
+
+for l in range(1,33):
+    print(l,"x",l,"=",l*l)
---
+++ P-ukol1.py
@@ -0,0 +1,9 @@
+# ctvercova cisla do 100
+
+print ("Ctvercova cisla do 100")
+#1x1=1
+#2x2=4
+#3x3=9
+
+for k in range (1,101):
+    print(k,"x",k,"=",k*k)
